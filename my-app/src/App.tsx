61b242b81bf53c3dfd209a4c1d99702e7979d36b
mgmgshake <pocky722+git@gmail.com>
2022-07-02 22:08:41 +0900

    inputにmuiを適応したところまで

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -1,34 +1,37 @@
-import React from "react";
-import { useForm, SubmitHandler } from "react-hook-form";
+import { useForm, SubmitHandler, Controller } from "react-hook-form";
+import InputText from "./components/input/Text/Text";
 
-type Inputs = {
-  example: string;
-  exampleRequired: string;
+export type IFormInput = {
+  firstName: string;
+  lastName: string;
 };
 
 export default function App() {
   const {
-    register,
+    control,
     handleSubmit,
     watch,
     formState: { errors },
-  } = useForm<Inputs>();
-  const onSubmit: SubmitHandler<Inputs> = (data) => console.log(data);
+  } = useForm<IFormInput>({
+    defaultValues: {
+      firstName: "test",
+    },
+    mode: "onSubmit",
+  });
+  const onSubmit: SubmitHandler<IFormInput> = (data) => console.log({ data });
 
-  console.log(watch("example")); // watch input value by passing the name of it
+  console.log(watch("firstName"));
 
   return (
-    /* "handleSubmit" will validate your inputs before invoking "onSubmit" */
     <form onSubmit={handleSubmit(onSubmit)}>
-      {/* register your input into the hook by invoking the "register" function */}
-      <input defaultValue="test" {...register("example")} />
-
-      {/* include validation with required or other standard HTML validation rules */}
-      <input {...register("exampleRequired", { required: true })} />
-      {/* errors will return when field validation fails  */}
-      {errors.exampleRequired && <span>This field is required</span>}
-
+      <InputText
+        control={control}
+        name="firstName"
+        rules={{ required: true }}
+      />
+      <InputText control={control} name="lastName" />
       <input type="submit" />
+      {errors.firstName && <span>error</span>}
     </form>
   );
 }
